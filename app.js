ba6d649d5f89ded6d9ca3ef08b227cf36239d3f7
Maria Souza <messenger.mariah2017@gmail.com>
2020-08-24 13:42:08 -0300

    estilização e ajuste do código

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-const fetchPokemon = () => {
-    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+const generatePokemonsPromises = () => Array(300).fill().map((_, index) =>
+fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 
-const pokemonPromises = []
+const generateHTML = pokemons =>  pokemons.reduce((accumulator, {name, id, types}) => {
+        const elementTypes = types.map(typeInfo => typeInfo.type.name)
 
+        accumulator += `
+        <li class="card ${elementTypes[0]}">
+        <img class="card-image " alt="${name}" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png"/>
+        <h2 class="card-title">${id}. ${name}</h2>
+        <p class="card-subtitle">${elementTypes.join(' | ')}</p>
+        </li>
+        `
+        return accumulator
 
-    for (let i = 1; i <= 150; i++){
-        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
-        
+    }, '')
+    
+    
 
+    const insertPokemonsIntoPage = pokemons=>{
+        const ul = document.querySelector('[data-js= "pokedex"]')
+
+        ul.innerHTML = pokemons
     }
+    
+const pokemonPromises = generatePokemonsPromises()
+Promise.all(pokemonPromises)
+    .then(generateHTML)
+    .then(insertPokemonsIntoPage)
 
-    Promise.all(pokemonPromises)
-    .then(pokemons =>{
-        // console.log(pokemons)
 
 
-        const lisPokemons = pokemons.reduce((accumulator, pokemon)=>{
-            accumulator += `<li>${pokemon.name}</li>`
-            return accumulator
 
-        }, '')
 
-        console.log(lisPokemons)
-    })
 
-}
+   
 
-fetchPokemon()
+
